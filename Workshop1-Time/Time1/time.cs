e28794f4842c51122c9f35caa7d5e73b87b3af33
Angie Manuela Marin <Angie Manuela Marin@ANG-2021>
2026-02-27 01:11:22 -0500

    implemented constructors to handle different time inputs

---
+++ Workshop1-Time/Time1/time.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Time1
+{
+    public class Time
+    {
+        // Private Atributes //
+        private int Ang_Hour;
+        private int Ang_Minute;
+        private int Ang_Second;
+        private int Ang_milliseconds;
+
+        // Default constructors//
+        public Time()
+        {
+            Ang_Hour = 0;
+            Ang_Minute = 0;
+            Ang_Second = 0;
+            Ang_milliseconds = 0;
+        }
+        // Constructor with hours //
+        public Time (int hour)
+        {
+            Ang_Hour = hour;
+        }
+        // Constructor with hour and minute //
+        public Time (int hour, int minute)
+        {
+            Ang_Hour = hour;
+            Ang_Minute = minute;
+        }
+        // Constructor whit hour, minute and seconds //
+        public Time (int hour, int minute, int seconds)
+        {
+            Ang_Hour = hour;
+            Ang_Minute = minute;
+            Ang_Second = seconds;
+        }
+        // Constructor whit hour, minute, seconds and millisecond //
+        public Time(int hour, int minute, int seconds, int milliseconds)
+        {
+            Ang_Hour = hour;
+            Ang_Minute = minute;
+            Ang_Second = seconds;
+            Ang_milliseconds = milliseconds;
+        }
+               
+
+
+    }
+}
